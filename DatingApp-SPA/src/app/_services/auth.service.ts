b3c9d6c81e1381cadc375d141189482cf6a9f73c
Avoaja Ugochukwu <karlthegoo@hotmail.com>
2018-12-25 10:55:21 +0100

    Display images of members and buttons

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -29,7 +29,7 @@ export class AuthService {
   }
 
   register(model: any) {
-    return this.http.post(this.baseUrl + 'register', model)
+    return this.http.post(this.baseUrl + 'register', model);
   }
 
   loggedIn() {
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -12,12 +12,14 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
+import { MembersCardComponent } from './members/members-card/members-card.component';
+
 
 
 
@@ -29,7 +31,8 @@ import { UserService } from './_services/user.service';
       RegisterComponent,
       MemberListComponent,
       MessagesComponent,
-      ListsComponent
+      ListsComponent,
+      MembersCardComponent
    ],
    imports: [
       BrowserModule,
